2a99cab865b0700b5c6ae41030d981f6abdff369
Zhenya Sinyakin <evgeny.siniakin@yandex.ru>
2023-08-10 17:26:40 +0300

    Первое задание правки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,7 @@
 "use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b ** 2 - 4 * a * c;
+  const arr = [];
+  const d = b ** 2 - 4 * a * c;
 
   if (d > 0) {
     arr[0] = (-b + Math.sqrt(d)) / (2 * a);
@@ -10,16 +10,13 @@ function solveEquation(a, b, c) {
   if (d == 0) {
     arr[0] = -b / (2 * a);
   }
-  if (d < 0) {
-    return arr;
-  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let rate = percent / 100 / 12; //п.2
   let sum = amount - contribution; //п.3
-  let payment = sum * (rate + rate / (((1 + rate) ^ countMonths) - 1)); //п.4
-  let fullSum = (payment * countMonths).toFixed(2); //п.5,6
-   
+  let payment = sum * (rate + rate / (((1 + rate) ** countMonths) - 1)); //п.4
+  let fullSum = (parseInt(payment * countMonths).toFixed(2)); //п.5,6
+   return;
 }
