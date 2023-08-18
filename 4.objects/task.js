3530c5b57987be6cc2ac91b92e138710191e3c98
Zhenya Sinyakin <evgeny.siniakin@yandex.ru>
2023-08-18 19:45:46 +0300

    Четвёртое задание - 2 пункта х

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    //п.1
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName; //п.2
 }
 
 Student.prototype.addMarks = function (...marks) {
+//п.3
+    if(this.hasOwnProperty(marks) === true) {
+        return 0;
+    } else {
+        this.marks.push(...marks);
+    }
   
 }
 
 Student.prototype.getAverage = function () {
-  
+  //п.4
+  if(this.hasOwmProperty(marks) === false || this.marks.length === 0) {
+    return 0;
+} else {
+    return (this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length);
+}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
