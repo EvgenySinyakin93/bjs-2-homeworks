02c3e2c1cc89a45e3fae86bf9fbbb2c4cb8de15d
Zhenya Sinyakin <evgeny.siniakin@yandex.ru>
2023-08-30 19:47:27 +0300

    Седьмое задание не полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,71 @@
+//Задача 1
+//п.1
+
+class AlarmClock {
+  constructor(alarmCollection, intervalId) {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  //п.2
+  addClock(time, callback){
+    if (!time || !callback) {
+      //проверяю переданы ли параметры времени и коллбека
+      throw new Error("Отсутствуют обязательные аргументы");
+    }
+    if (this.alarmCollection.find((setup) => setup.time === time)) {
+      console.warn("Уже присутствует звонок на это же время"); //проверяем есть ли звонок в то же время
+    }
+    this.alarmCollection.push({
+      callback,
+      time,
+      canCall: true,
+    }); //добавляем в массив звонков объект со свойствами
+  };
+
+  //п.3
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(
+      (setup) => setup.time != time
+    );
+  } //удаляем одинаковые звонки из массива
+
+  //п.4
+  getCurrentFormattedTime() {
+    return new Date.parse("HH:MM");
+  }
+
+  //п.5
+  start(){
+    if(this.intervalId){ //если есть значения, то завершаем
+        return;
+    }
+    this.intervalId = newIntervalid(() => {
+            this.alarmCollection.forEach(setup => { //перебираем через foreach
+                if(setup.time === this.getCurrentFormattedTime && setup.canCall){ //проверяем совпадение time и выполнение звонка
+                    setup.canCall = false; //делаем возомжность вызова звонка false
+                    setup.callback();//вызываес коллбек звонка
+                }
+            })
+        })
+    }
+
+    //п.6
+    stop(){
+        clearInterval(this.intervalId);//вызвали функцию для удаления интервала
+        this.intervalId = null; //для сброса присвоили значени
+    }
+    //п.7
+    resetAllCalls(){
+        this.alarmCollection.forEach(setup => setup.canCall === true); //присваивайем true
+    }
+
+    //п.8
+    clearAlarms(){
+        this.intervalId.stop(); //метод остановки интервала
+        this.alarmCollection = []; //удаляем все звонки
+    }
+
+  }
+
+
