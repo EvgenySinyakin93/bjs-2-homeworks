768219e3a39f2d07d026b6f0b6b0e8b28bbb7bda
Zhenya Sinyakin <evgeny.siniakin@yandex.ru>
2023-08-31 15:39:10 +0300

    Седьмое задание не полностью готово

--- 7.async/task.js
+++ 7.async/task.js
@@ -2,13 +2,13 @@
 //п.1
 
 class AlarmClock {
-  constructor(alarmCollection, intervalId) {
+  constructor() {
     this.alarmCollection = [];
     this.intervalId = null;
   }
 
   //п.2
-  addClock(time, callback){
+  addClock(time, callback) {
     if (!time || !callback) {
       //проверяю переданы ли параметры времени и коллбека
       throw new Error("Отсутствуют обязательные аргументы");
@@ -21,7 +21,7 @@ class AlarmClock {
       time,
       canCall: true,
     }); //добавляем в массив звонков объект со свойствами
-  };
+  }
 
   //п.3
   removeClock(time) {
@@ -32,40 +32,43 @@ class AlarmClock {
 
   //п.4
   getCurrentFormattedTime() {
-    return new Date.parse("HH:MM");
+    return new Date().toLocaleTimeString("ru-Ru", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
   }
 
   //п.5
-  start(){
-    if(this.intervalId){ //если есть значения, то завершаем
-        return;
+  start() {
+    if (this.intervalId) {
+      //если есть значения, то завершаем
+      return;
     }
-    this.intervalId = newIntervalid(() => {
-            this.alarmCollection.forEach(setup => { //перебираем через foreach
-                if(setup.time === this.getCurrentFormattedTime && setup.canCall){ //проверяем совпадение time и выполнение звонка
-                    setup.canCall = false; //делаем возомжность вызова звонка false
-                    setup.callback();//вызываес коллбек звонка
-                }
-            })
-        })
-    }
-
-    //п.6
-    stop(){
-        clearInterval(this.intervalId);//вызвали функцию для удаления интервала
-        this.intervalId = null; //для сброса присвоили значени
-    }
-    //п.7
-    resetAllCalls(){
-        this.alarmCollection.forEach(setup => setup.canCall === true); //присваивайем true
-    }
-
-    //п.8
-    clearAlarms(){
-        this.intervalId.stop(); //метод остановки интервала
-        this.alarmCollection = []; //удаляем все звонки
-    }
-
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach((setup) => {
+        //перебираем через foreach
+        if (setup.time === this.getCurrentFormattedTime() && setup.canCall) {
+          //проверяем совпадение time и выполнение звонка
+          setup.canCall = false; //делаем возомжность вызова звонка false
+          setup.callback(); //вызываес коллбек звонка
+        }
+      });
+    });
   }
 
+  //п.6
+  stop() {
+    clearInterval(this.intervalId); //вызвали функцию для удаления интервала
+    this.intervalId = null; //для сброса присвоили значени
+  }
+  //п.7
+  resetAllCalls() {
+    this.alarmCollection.forEach((setup) => setup.canCall === true); //присваивайем true
+  }
 
+  //п.8
+  clearAlarms() {
+    stop(); //метод остановки интервала
+    this.alarmCollection = []; //удаляем все звонки
+  }
+}
